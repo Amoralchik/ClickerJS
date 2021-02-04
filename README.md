7752397e08579ba824330bef0f9ce88cf191e51b
Amoralchik <dariysdemonad@gmail.com>
2021-02-05 01:06:16 +0200

    Version: 0.9.1 README.md

---
+++ README.md
@@ -0,0 +1,11 @@
+--Version 0.7.5 
+    Start
+
+--Version 0.7.6
+    Add git - fix
+
+--Version 0.8.2
+    White theme
+
+--Version 0.9.1
+    White theme reColor, fix, README.md, Gamemode Selector, Gamemode: 5sec | 10sec | 25sec | idle(wip)
--- main.js
+++ main.js
@@ -7,11 +7,9 @@ const userDB = {
     score: scoreDB
 };
 
+let timeout = 5000;
+
 let scoreTop = [];
-
-let timerout = 5000
-
-const timeout = timerout;
 
 const display = document.querySelector('#display'),
       timeDisplay = document.querySelector('#time__display')
@@ -20,19 +18,42 @@ const display = document.querySelector('#display'),
       restartButton = document.querySelector('#restart'),
       winterButton = document.querySelector('#winter'),
       bodyHtml = document.querySelector("#body"),
-      mediaText = document.querySelector("#mediaText");
+      mediaText = document.querySelector("#mediaText"),
+      gameModeSelector = document.querySelector("#gameMode");
+
+const gameModeChenge = ["Classic", "10sec", "25sec", "idle"];
+let gmNum = 0;
+let gameMode = gameModeChenge[gmNum];
+
+let gameModeFunc = () => {
+    gmNum += 1;
+    gameMode =  gameModeChenge[gmNum];
+    console.log(gmNum);
+    gameModeSelector.innerHTML = `${gameMode}`;
+    if (gmNum > 2) {
+        gmNum = -1;
+    };
+    timeout =  gmNumberFix();
+    console.log(timerout);
+};
+
+gameModeSelector.onclick = gameModeFunc;
 
 button.onclick = start;
 
 let themeWinterOn = true;
 
-winterButton.onclick = () => {
+winterButton.addEventListener( "click" , themeChenge);
+
+function themeChenge() {
     if (themeWinterOn) {
         restartButton.classList.add("winter__restart")
         winterButton.classList.add("winter__restart")
         button.classList.add("winter__button")
         bodyHtml.classList.add("winter")
         mediaText.classList.add("winter")
+        gameModeSelector.classList.add("winter__restart")
+        winterButton.textContent = "Light off";
         themeWinterOn = false;
     } else {
         restartButton.classList.remove("winter__restart")
@@ -40,6 +61,8 @@ winterButton.onclick = () => {
         button.classList.remove("winter__button")
         bodyHtml.classList.remove("winter")
         mediaText.classList.remove("winter")
+        gameModeSelector.classList.remove("winter__restart")
+        winterButton.textContent = "Light on";
         themeWinterOn = true;
     }
     
@@ -55,7 +78,24 @@ function restart() {
     restartButton.onclick = false;
 };
 
+function  gmNumberFix() {
+
+    if (gmNum == 0 || gmNum == 1 || gmNum == 2) {
+        if(gmNum == 0) {
+            timerout = 5000;
+        } else if (gmNum == 1) {
+            timerout = 10000;
+        } else {
+            timerout = 25000;
+        }
+    };
+    return timerout;
+};
+
 function start() {
+
+    gameModeSelector.classList.add("hide")
+
     const startTime = Date.now();
 
     button.textContent = "Clicker";
@@ -79,7 +119,7 @@ function start() {
         button.textContent = 'Game Over';
 
         userDB.score = Math.trunc(clicks);
-        scoreTop.push({user: userDB.user ,score: userDB.score});
+        scoreTop.push({user: userDB.user, score: userDB.score, game: gameModeChenge[gmNum]});
         
         function sortByAge(arr) {
             arr.sort((a, b) => a.score < b.score ? 1 : -1);
@@ -89,9 +129,14 @@ function start() {
 
         counter.innerHTML = '';
         for (let item = 0; item < scoreTop.length && item < 10; item++) {
-            counter.innerHTML += `<li class=""> ${scoreTop[item].user} - score: ${scoreTop[item].score} </li>`;   
+            counter.innerHTML += `
+            <li class="">
+            ${scoreTop[item].user} - score: ${scoreTop[item].score} |
+            Game mode: ${scoreTop[item].game} 
+            </li>`;   
         };  
 
+        gameModeSelector.classList.remove("hide")
         restartButton.classList.remove("hide")
         restartButton.onclick = restart;
 
